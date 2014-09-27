2d2e5a4a41a4417e30d6e7be2aa02267a4e5fa40
Isaac Chansky <ichansky@gmail.com>
2014-09-26 22:49:29 -0400

    bind view/events to completion and task summary

--- client/views/todo.js
+++ client/views/todo.js
@@ -15,13 +15,45 @@ module.exports = View.extend({
             type: 'attribute',
             hook: 'task',
             name: 'href'
-        }
+        },
+        'model.summary': {
+            type: 'value',
+            hook: 'summary'
+        },
+        'model.isComplete': [
+            {
+                type: 'booleanClass',
+                hook: 'complete',
+                name: 'complete',
+                yes: 'completed',
+                no: 'incomplete'
+            },
+            {
+                type: 'booleanAttribute',
+                hook: 'action-complete',
+                name: 'checked',
+            }
+        ],
     },
     events: {
-        'click [data-hook~=action-delete]': 'handleRemoveClick'
+        'click [data-hook~=action-delete]': 'handleRemoveClick',
+        'change [data-hook~=action-complete]': 'toggleComplete',
+        'click [data-hook~=action-update-summary]': 'updateSummary'
     },
     handleRemoveClick: function () {
         this.model.destroy();
         return false;
+    },
+    toggleComplete: function (e) {
+        if(this.model.isComplete){
+            this.model.isComplete = false;
+        }else{
+            this.model.isComplete = true;
+        }
+        this.model.save();
+    },
+    updateSummary: function(){
+        this.model.summary = this.queryByHook('summary').value;
+        this.model.save();
     }
 });
